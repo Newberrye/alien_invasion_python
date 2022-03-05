bd638c339c34c8d18f90268ddb9f4abc4cd4bb82
EthanN <newberrydethan@gmail.com>
2022-03-05 13:29:12 -0600

    Added bullet deletion, bullet limit, and refactored bullet handling into its own method

--- alien_invasion.py
+++ alien_invasion.py
@@ -33,8 +33,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
-
+            self._update_bullets()
             self._update_screen()
 
     def _check_events(self):
@@ -65,8 +64,19 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         # Creates bullet and adds to bullet group.
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullet_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        # Update position of bullets and gets rid of old bullets.
+        # Update bullet positions
+        self.bullets.update()
+
+        # Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _update_screen(self):
         # Updates images on the screen, and flips to the new screen
