9d7f7138679026581e8e835c278ae31a3bf3bd4c
EthanN <newberrydethan@gmail.com>
2022-03-05 16:33:52 -0600

    Made a few changes to alien invasion trying to fix check_edges error; unsuccessful

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,11 +2,10 @@ import sys
 
 import pygame
 
+from alien import Alien
+from bullet import Bullet
 from settings import Settings
 from ship import Ship
-from bullet import Bullet
-from alien import Alien
-
 
 class AlienInvasion:
     # Overall class to manage game assets and behavior
@@ -38,6 +37,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
     def _check_events(self):
@@ -82,6 +82,11 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+    def _update_aliens(self):
+        # Update the positions of all aliens in the fleet
+        self._check_fleet_edges()
+        self.aliens.update()
+
     def _update_screen(self):
         # Updates images on the screen, and flips to the new screen
         self.screen.fill(self.settings.bg_color)
@@ -120,6 +125,19 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
+
+    def _check_fleet_edges(self):
+        # Respond appropriately if any aliens have reached an edge.
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        # Drops entire fleet and change fleet direction.
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+            self.settings.fleet_direction *= -1
 
 
 if __name__ == '__main__':
