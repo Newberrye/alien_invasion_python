13f838d43c975e5862ae2c8c2e5c639133c690fe
EthanN <newberrydethan@gmail.com>
2022-03-05 10:26:43 -0600

    Added ship movement, ship movement limit, speed setting, refactored check events, and added quit with q.

--- ship.py
+++ ship.py
@@ -5,6 +5,7 @@ class Ship:
     # A class to manage the ship.
     def __init__(self, ai_game):
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image get its rect.
@@ -12,6 +13,24 @@ class Ship:
         self.rect = self.image.get_rect()
         # Start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Store a decimal value for ship's horizontal position
+        self.x = float(self.rect.x)
+
+        # Movement flags
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        # Updates the ship's position based on the movement flags.
+        # Updates ship's x value, not actual screen position
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x
+        self.rect.x = self.x
 
     def blitme(self):
         # Draw the ship at the current location.
