9927bf6cffa54372d60b9ee53241e43a855bc865
EthanN <newberrydethan@gmail.com>
2022-03-04 22:32:52 -0600

    Refactored event getter and screen updater from run method to their own methods.

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,18 +24,21 @@ class AlienInvasion:
     def run_game(self):
         # Start main loop for the game.
         while True:
-            # Watches for keyboard and mouse events
-            for event in pygame.event.get():
-                # Watches for windows exit button
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            # Redraws screen during each loop pass
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+    def _check_events(self):
+        # Responds to keyboard and mouse events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-            # Make the most recently drawn screen visible
-            pygame.display.flip()
+    def _update_screen(self):
+        # Updates images on the screen, and flips to the new screen
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
